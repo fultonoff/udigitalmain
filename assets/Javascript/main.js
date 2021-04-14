6a89719f44401551ac1eac3fa6e0db7757d88f86
fulton <fultonkilebe7@gmail.com>
2021-04-14 13:40:49 +0300

    header

--- assets/Javascript/main.js
+++ assets/Javascript/main.js
@@ -4,16 +4,36 @@
 const toogle = document.querySelector('.toggle');
 const nav =document.querySelector('.list');
 const back = document.querySelector('.background');
-const btn = document.querySelector('.btn')
+const btn = document.querySelector('.btn');
+const body = document.body;
 
 
 
 toogle.addEventListener('click', (e) =>{
-
+    
    
     nav.classList.toggle('active');
     back.classList.toggle('active');
     btn.classList.toggle('active');
     toogle.classList.toggle('active');
+    
 
 })
+
+//////////////////////////////////////////////////////
+///// client slide
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 8,
+        loop: true,
+        autoWidth:true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        slideTransition: 'linear',
+        autoplaySpeed: 3000,
+        autoplayHoverPause: false,
+        
+
+        
+});
