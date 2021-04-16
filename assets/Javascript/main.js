f790e7611880b1cc3add3b22166d505f11eeb033
fulton <fultonkilebe7@gmail.com>
2021-04-16 19:14:42 +0300

    tesmony

--- assets/Javascript/main.js
+++ assets/Javascript/main.js
@@ -23,21 +23,43 @@ toogle.addEventListener('click', (e) =>{
 //////////////////////////////////////////////////////
 ///// client slide
 
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    items: 1,
-        loop: true,
-        autoWidth:true,
-        center: true,
-        autoplay: true,
-        autoplayTimeout: 4000,
-        slideTransition: 'linear',
-        autoplaySpeed: 4000,
-        autoplayHoverPause: false,
-        dots: false
+// var owl = $('.owl-carousel');
+// owl.owlCarousel({
+//     items: 1,
+//         loop: true,
+//         autoWidth:true,
+//         center: true,
+//         autoplay: true,
+//         autoplayTimeout: 4000,
+//         slideTransition: 'linear',
+//         autoplaySpeed: 4000,
+//         autoplayHoverPause: false,
+//         dots: false
 
        
         
 
         
-});
+// });
+
+
+
+$(document).ready(function() {
+ 
+    $("#owl-tesmony").owlCarousel({
+   
+        navigation : true, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true,
+        center: true,
+        items : 1, 
+        itemsDesktop : false,
+        itemsDesktopSmall : false,
+        itemsTablet: false,
+        itemsMobile : false,
+        autoplay: true
+   
+    });
+   
+  });
